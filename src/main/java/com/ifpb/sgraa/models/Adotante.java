ed3dc256902919de88fcd310cb45e16ef971e507
João Marcos <marcosjmorim@gmail.com>
2025-02-14 10:11:42 -0300

    feat(Adotante): criando metodo de solicitarAdocao
    - Alteracoes no enum de StatusAdocao para englobar mais casos

--- src/main/java/com/ifpb/sgraa/models/Adotante.java
+++ src/main/java/com/ifpb/sgraa/models/Adotante.java
@@ -8,24 +8,38 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ifpb.sgraa.enums.StatusAdocao;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Adotante {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private String nome;
+  @Column(nullable = false)
+  private String nome;
 
-    @Column(unique = true)
-    private String cpf;
+  @Column(unique = true)
+  private String cpf;
 
-    private String endereco;
-    private String contato;
+  private String endereco;
+  private String contato;
 
-    @OneToMany(mappedBy = "adotante")
-    private List<Adocao> adocoes = new ArrayList<>();
-}
+  @OneToMany(mappedBy = "adotante")
+  private List<Adocao> adocoes = new ArrayList<>();
+
+  public Adocao solicitarAdocao(Animal animal) {
+    if (animal.getStatusAdocao() == StatusAdocao.INDISPONIVEL)
+      return null;
+    Adocao adocao = new Adocao();
+    adocao.setAdotante(this);
+    adocao.setAnimal(animal);
+    animal.setStatusAdocao(StatusAdocao.SOLICITADO);
+    adocoes.add(adocao);
+    return adocao;
+  }
+
+  }
