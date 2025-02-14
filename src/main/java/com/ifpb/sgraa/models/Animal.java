69b0c6b906339cf635fa4048e3e192bac4eeafca
João Marcos <marcosjmorim@gmail.com>
2025-02-14 09:33:34 -0300

    feat: adicionando metodos de Animal

--- src/main/java/com/ifpb/sgraa/models/Animal.java
+++ src/main/java/com/ifpb/sgraa/models/Animal.java
@@ -4,9 +4,7 @@ import com.ifpb.sgraa.enums.EspecieAnimal;
 import com.ifpb.sgraa.enums.StatusAdocao;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,36 +14,44 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Animal {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private String nome;
+  @Column(nullable = false)
+  private String nome;
 
-    @Enumerated(EnumType.STRING)
-    private EspecieAnimal especie;
+  @Enumerated(EnumType.STRING)
+  private EspecieAnimal especie;
 
-    private String raca;
-    private int idade;
+  private String raca;
+  private int idade;
 
-    @Enumerated(EnumType.STRING)
-    private StatusAdocao statusAdocao;
+  @Enumerated(EnumType.STRING)
+  private StatusAdocao statusAdocao;
 
-    @Temporal(TemporalType.DATE)
-    private Date dataEntrada;
+  @Temporal(TemporalType.DATE)
+  private Date dataEntrada;
 
-    @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<TratamentoMedico> tratamentos = new ArrayList<>();
+  @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<TratamentoMedico> tratamentos = new ArrayList<>();
 
-    @OneToOne(mappedBy = "animal")
-    private Adocao adocao;
+  @OneToOne(mappedBy = "animal")
+  private Adocao adocao;
 
-    @ManyToOne
-    @JoinColumn(name = "resgate_id")
-    private Resgate resgate;
+  @ManyToOne
+  @JoinColumn(name = "resgate_id")
+  private Resgate resgate;
 
-    @ManyToMany(mappedBy = "animaisAlimentados")
-    private List<Estoque> alimentos = new ArrayList<>();
+  @ManyToMany(mappedBy = "animaisAlimentados")
+  private List<Estoque> alimentos = new ArrayList<>();
+
+  public void atualizarStatus(StatusAdocao status) {
+    this.statusAdocao = status;
+  }
+
+  public void registrarSaida() {
+    this.statusAdocao = StatusAdocao.ADOTADO;
+  }
+
 }
-
